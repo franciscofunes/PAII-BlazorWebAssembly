fc434f3ed862e85da55c9e3ad4b6d401aade54ee
HEXACTA\ffunes <ffunes@hexacta.com>
2023-09-22 09:26:29 -0300

    add mobile menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,20 @@ module.exports = {
       fontFamily: {
         custom: ['Helvetica', 'sans'],
       },
+      animation: {
+        pulse: 'pulse 1s cubic-bezier(0.4, 0, 0.6, 1) 1',
+      },
+      keyframes: {
+        pulse: {
+          '0%, 100%': {
+            opacity: 1,
+          },
+          '80%': {
+            opacity: 0.9,
+          },
+        },
+      },
     },
   },
   plugins: [],
 };
-
-
